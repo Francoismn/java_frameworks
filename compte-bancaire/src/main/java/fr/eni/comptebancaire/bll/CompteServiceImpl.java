69d99812c355d822530ae908d0c5487ca561ef09
Francois MOISAN <francois.moisan2024@campus-eni.fr>
2024-09-10 15:33:38 +0200

    demo compte bancaire fin

---
+++ compte-bancaire/src/main/java/fr/eni/comptebancaire/bll/CompteServiceImpl.java
@@ -0,0 +1,51 @@
+package fr.eni.comptebancaire.bll;
+
+import fr.eni.comptebancaire.bo.Compte;
+import fr.eni.comptebancaire.dal.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class CompteServiceImpl implements CompteService{
+
+    @Autowired
+    private CompteRepository compteRepository;
+
+    @Override
+    public void ajouterCompte(Compte compte) {
+        //Vérification avant
+        compteRepository.save(compte);
+    }
+
+    @Override
+    public Optional<Compte> recupererCompteParNum(String numCompte) {
+        return compteRepository.findByNumCompte(numCompte);
+    }
+
+    @Override
+    public void crediter(Compte compte, double montant) {
+        //Vérification avant
+        compte.setSolde(compte.getSolde() + montant);
+        compteRepository.save(compte);
+    }
+
+    @Override
+    public void debiter(Compte compte, double montant) throws Exception {
+        //Vérification
+        if(compte.getSolde() < montant) throw new Exception("Solde insuffisant");
+        compte.setSolde(compte.getSolde() - montant);
+        compteRepository.save(compte);
+
+    }
+
+    @Transactional
+    @Override
+    public void virement(Compte emetteur, Compte beneficiaire, double montant) throws Exception {
+        debiter(emetteur, montant);
+        crediter(beneficiaire, montant);
+
+    }
+}
---
+++ compte-bancaire/src/test/java/fr/eni/comptebancaire/TestCompteService.java
@@ -0,0 +1,104 @@
+package fr.eni.comptebancaire;
+
+import fr.eni.comptebancaire.bll.CompteService;
+import fr.eni.comptebancaire.bo.Compte;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TestCompteService {
+
+    @Autowired
+    private CompteService compteService;
+
+    @Test
+    public void testSaveCompte() {
+
+        Compte compteLucas = Compte.builder()
+                .numCompte("FR87654321")
+                .solde(0)
+                .pseudo("Lucas")
+                .build();
+
+        Compte compteGaelle = Compte.builder()
+                .numCompte("FR12345678")
+                .solde(0)
+                .pseudo("Gaelle")
+                .build();
+
+        Compte compteFrancois = Compte.builder()
+                .numCompte("FR000001")
+                .solde(0)
+                .pseudo("Francois")
+                .build();
+
+        compteService.ajouterCompte(compteLucas);
+        compteService.ajouterCompte(compteGaelle);
+        compteService.ajouterCompte(compteFrancois);
+
+        Assertions.assertThat(compteLucas).isNotNull();
+        Assertions.assertThat(compteGaelle).isNotNull();
+        Assertions.assertThat(compteFrancois).isNotNull();
+
+    }
+
+    @Test
+    public void testCrediter() {
+        Optional<Compte> lucasCompteOpt = compteService.recupererCompteParNum("FR12345678");
+
+        double oldSolde = 0;
+        if (lucasCompteOpt.isPresent()){
+            oldSolde = lucasCompteOpt.get().getSolde();
+        }
+        Assertions.assertThat(lucasCompteOpt).isPresent();
+        Compte lucasCompte = lucasCompteOpt.get();
+        compteService.crediter(lucasCompte, 10000);
+        Assertions.assertThat(lucasCompte.getSolde()).isEqualTo(oldSolde + 10000);
+    }
+
+    @Test
+    public void testDebiter() throws Exception {
+        Optional<Compte> lucasCompteOpt = compteService.recupererCompteParNum("FR87654321");
+
+        double oldSolde = 0;
+        if (lucasCompteOpt.isPresent()){
+            oldSolde = lucasCompteOpt.get().getSolde();
+        }
+        Assertions.assertThat(lucasCompteOpt).isPresent();
+        Compte lucasCompte = lucasCompteOpt.get();
+        try{
+            compteService.debiter(lucasCompte, 500);
+            Assertions.assertThat(lucasCompte.getSolde()).isEqualTo(oldSolde - 500);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testVirement() {
+        Optional<Compte> lucasCompteOpt = compteService.recupererCompteParNum("FR87654321");
+        Optional<Compte> gaelleCompteOpt = compteService.recupererCompteParNum("FR12345678");
+
+        Assertions.assertThat(lucasCompteOpt).isPresent();
+        Assertions.assertThat(gaelleCompteOpt).isPresent();
+
+        try {
+            compteService.virement(lucasCompteOpt.get(),gaelleCompteOpt.get(), 1000);
+            Assertions.assertThat(lucasCompteOpt.get().getSolde()).isEqualTo(2500);
+            Assertions.assertThat(gaelleCompteOpt.get().getSolde()).isEqualTo(1000);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
