bc00c04c26a85ac0eca1c1d226e6b50b3fc632b5
Francois MOISAN <francois.moisan2024@campus-eni.fr>
2024-09-11 12:34:49 +0200

    demo twitter onetomany

---
+++ Twitter/src/main/java/fr/eni/twitter/bo/Comment.java
@@ -0,0 +1,25 @@
+package fr.eni.twitter.bo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+    private LocalDateTime createdAt;
+
+}
