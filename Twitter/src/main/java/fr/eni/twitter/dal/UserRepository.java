b2006093fb646d0587e1c1817872a3a3c7809eb6
Francois MOISAN <francois.moisan2024@campus-eni.fr>
2024-09-13 14:08:01 +0200

    demo twitter requetes

--- Twitter/src/main/java/fr/eni/twitter/dal/UserRepository.java
+++ Twitter/src/main/java/fr/eni/twitter/dal/UserRepository.java
@@ -2,6 +2,21 @@ package fr.eni.twitter.dal;
 
 import fr.eni.twitter.bo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //Java Persistence Query Language
+    @Query("SELECT u FROM User u WHERE u.username LIKE :keyword")
+    List<User> searchUserByUsername(@Param("keyword") String keyword);
+
+
+    @Query(value = "SELECT u.* FROM users u WHERE u.email = :keyword", nativeQuery = true)
+    Optional<User> searchUserByEmail(@Param("keyword") String keyword);
+
+
 }
--- Twitter/src/test/java/fr/eni/twitter/TwitterApplicationTests.java
+++ Twitter/src/test/java/fr/eni/twitter/TwitterApplicationTests.java
@@ -3,20 +3,28 @@ package fr.eni.twitter;
 import fr.eni.twitter.bo.Profile;
 import fr.eni.twitter.bo.User;
 import fr.eni.twitter.dal.UserRepository;
+import jakarta.persistence.Column;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TwitterApplicationTests {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
+    @Commit
     void contextLoads() {
         Profile profile = Profile.builder()
                 .firstname("Pierre")
@@ -32,7 +40,39 @@ class TwitterApplicationTests {
 
         User userBdd = userRepository.save(user);
         Assertions.assertThat(userBdd).isNotNull();
+
         System.out.println(userBdd.getProfile());
     }
+
+    //SQL
+    @Test
+    public void testSearchUserByUsername() {
+        //Arrange
+        String motCle = "%Pierre%";
+        //Act
+        List<User> users = userRepository.searchUserByUsername(motCle);
+        //Assert
+        Assertions.assertThat(users).hasSize(1);
+
+        System.out.println(users);
+    }
+
+    //SQL
+    @Test
+    public void testSearchUserByEmail() {
+        //Arrange
+        String motCle = "pierre@mail.com";
+        //Act
+        Optional<User> users = userRepository.searchUserByEmail(motCle);
+        //Assert
+        Assertions.assertThat(users).isNotNull();
+
+        System.out.println(users);
+    }
+
+
+
+
+
 
 }
