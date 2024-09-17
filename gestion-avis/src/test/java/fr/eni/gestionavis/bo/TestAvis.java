232c4bbcc851c54b6fbe2ee1e4ce4b3d136f4532
Francois MOISAN <francois.moisan2024@campus-eni.fr>
2024-09-17 11:27:23 +0200

    TP4

--- gestion-avis/src/test/java/fr/eni/gestionavis/bo/TestAvis.java
+++ gestion-avis/src/test/java/fr/eni/gestionavis/bo/TestAvis.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -22,22 +21,18 @@ public class TestAvis {
 
     @Test
     public void saveReview(){
-        // Créer un client
         Client client = Client.builder()
                 .pseudo("bobeponge@email.fr")
                 .quantiteCommandee(11)
                 .build();
 
-        // Créer une bouteille
         Bouteille bouteille = Bouteille.builder()
                 .id(new BouteilleId(2298, 5, 1))
                 .nom("Bordeaux Supérieur")
                 .build();
 
-        // Sauvegarder la bouteille dans le repository
         bouteilleRepository.save(bouteille);
 
-        // Créer un avis avec le client et la bouteille
         Avis avis = Avis
                 .builder()
                 .note(5)
@@ -47,27 +42,22 @@ public class TestAvis {
                 .bouteille(bouteille)  // Association avec la bouteille
                 .build();
 
-        // Sauvegarder l'avis dans le repository
         Avis savedAvis = avisRepository.save(avis);
 
-        // Vérifier que l'avis a bien été sauvegardé avec ses associations
         Assertions.assertThat(savedAvis.getId()).isNotNull();
         Assertions.assertThat(savedAvis.getClient()).isNotNull();
         Assertions.assertThat(savedAvis.getBouteille()).isNotNull();
 
-        // Afficher l'avis pour vérifier son contenu
         System.out.println(savedAvis);
     }
 
     @Test
     public void recoverReviews(){
-        List<Avis> avisList = avisRepository.findAll(); // Récupérer tous les avis depuis le repository
+        List<Avis> avisList = avisRepository.findAll();
 
-        // Vérifier que la liste des avis n'est pas vide
         Assertions.assertThat(avisList).isNotNull();
         Assertions.assertThat(avisList).isNotEmpty();
 
-        // Afficher la liste des avis
         avisList.forEach(System.out::println);
     }
 }
